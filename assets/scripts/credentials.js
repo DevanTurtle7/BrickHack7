1922e0c828c74e69994ba8ace4b23aa1273c64ce
Devan Kavalchek <devankav7@gmail.com>
2021-02-20 13:13:46 -0500

    added files and imports for firebase

--- assets/scripts/credentials.js
+++ assets/scripts/credentials.js
@@ -1,5 +1,8 @@
 // Not so secret :/
 
+// Spotify
 var client_id = "d661dc99991846f18487325c0815727d";
 var client_secret = "371a62c4772f420d8c1bfd9feafa1d02";
 var redirect_uri = "https://devanturtle7.github.io/BrickHack7/";
+
+// Firebase
