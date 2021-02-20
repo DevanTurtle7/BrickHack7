1ac6bcd521da8018ac6e2aa6d584cfc47559ca30
Devan Kavalchek <devankav7@gmail.com>
2021-02-20 13:27:32 -0500

    updated firebase scripts

--- assets/scripts/credentials.js
+++ assets/scripts/credentials.js
@@ -6,3 +6,12 @@ var client_secret = "371a62c4772f420d8c1bfd9feafa1d02";
 var redirect_uri = "https://devanturtle7.github.io/BrickHack7/";
 
 // Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyDGz4ngx2bkvFcQ_MfhRQG73Dy6eIJnX3s",
+    authDomain: "brickhack7.firebaseapp.com",
+    projectId: "brickhack7",
+    storageBucket: "brickhack7.appspot.com",
+    messagingSenderId: "1034835329070",
+    appId: "1:1034835329070:web:440ec09df35940b4935fbf",
+    measurementId: "G-6X89WNGPR3"
+};
--- assets/scripts/firestore.js
+++ assets/scripts/firestore.js
@@ -1,14 +1,7 @@
- // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-    apiKey: "AIzaSyDGz4ngx2bkvFcQ_MfhRQG73Dy6eIJnX3s",
-    authDomain: "brickhack7.firebaseapp.com",
-    projectId: "brickhack7",
-    storageBucket: "brickhack7.appspot.com",
-    messagingSenderId: "1034835329070",
-    appId: "1:1034835329070:web:440ec09df35940b4935fbf",
-    measurementId: "G-6X89WNGPR3"
-};
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+
+function setupFirebase() {
+    firebase.initializeApp(firebaseConfig);
+    db = firebase.firestore();
+
+    return db;
+}
