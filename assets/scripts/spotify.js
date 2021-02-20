0e47a3670058c55a9cda3f75f2677435603efb4b
Devan Kavalchek <devankav7@gmail.com>
2021-02-20 13:46:38 -0500

    added gitcode

--- assets/scripts/spotify.js
+++ assets/scripts/spotify.js
@@ -2,17 +2,24 @@ function spotifyLogin() {
     location.replace('https://accounts.spotify.com/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirect_uri + '&scope=user-read-currently-playing');
 }
 
-function login() {
+function getCode() {
     var responseQuery = window.location.search // Get the code in the URL returned from the Spotify login
     // The regex for getting the code
     var re = /[&?]code=([^&]*)/
-    var code
 
     try {
-        code = re.exec(responseQuery)[1] // Get the code
+        var code = re.exec(responseQuery)[1] // Get the code
+        return code;
     } catch {
-        // The window location is empty. Login to spotify
-        spotifyLogin()
+        return null;
+    }
+}
+
+function login() {
+    var code = getCode();
+
+    if (code == null) {
+        spotifyLogin();
     }
 }
 
