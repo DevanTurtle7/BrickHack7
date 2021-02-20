9f029bd967e512164b231c02e7df730d7a6605f0
Devan Kavalchek <devankav7@gmail.com>
2021-02-20 13:30:56 -0500

    put client secret in cloud

--- assets/scripts/firestore.js
+++ assets/scripts/firestore.js
@@ -5,3 +5,21 @@ function setupFirebase() {
 
     return db;
 }
+
+async function getClientSecret() {
+    /*
+    Gets the client secret from firestore
+    */
+    var docRef = db.collection('credentials').doc('client_secret')
+
+    var data = await docRef.get().then(function (doc) {
+        if (doc.exists) {
+            return doc.data()
+        }
+    }).catch(function (error) {
+        console.log('Error occurred getting secret. Trying again...')
+        return getClientSecret()
+    })
+
+    return data.value
+}
