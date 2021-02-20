968a51ddf1f92536c51a32da2a4399b9d1c74dae
Sean Kannanaikal <sean@seans-mbp.wireless.rit.edu>
2021-02-20 18:25:56 -0500

    added mkake room button

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
     var database = setupFirebase();
     login(database);
     
-    $( "#joinGroup" ).click(function() {
+    $("#joinGroup").click(function() {
         joinRoom($("#groupID").val());
+    });
+
+    $("#makeGroup").click(function() {
+        makeRoom();
     });
 });
 
