2f48c9fc6d1a30950ff55e16e73b6a6e6e267a98
Sean Kannanaikal <sean@seans-mbp.wireless.rit.edu>
2021-02-21 11:46:10 -0500

    made some stuff

--- assets/scripts/firestore.js
+++ assets/scripts/firestore.js
@@ -109,7 +109,6 @@ async function joinRoom(roomCode, database) {
 
     $("#addSong").click(async function () {
         addSongDB(database, roomCode, accessToken);
-        $("#songQueue").css("value", "");
     });
 
     var data = await getRoomData(database, roomCode);
